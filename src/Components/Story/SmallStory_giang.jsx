8bfb2adc3feb32a333f1eac4295c95c910d24fdd
Tragiang118 <2251172322@e.tlu.edu.vn>
2025-06-05 01:34:47 +0700

    Giao diện xem danh sách tin đã đăng

---
+++ src/Components/Story/SmallStory_giang.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { IoTimeOutline } from "react-icons/io5";
+
+const SmallStory_giang = ({storyImg, time}) => {
+  return (
+    <div
+      style={{ backgroundImage: `url(${storyImg})` }}
+      className="rounded-[20px] bg-cover bg-center w-full relative aspect-[3/4] p-[20px]"
+    >
+
+      <button className="absolute top-3 right-3 z-10 text-xs font-bold text-[--color-violet] bg-white px-2 py-0.5 rounded-[20px] shadow-md hover:shadow-lg hover:bg-gray-100 transition-all duration-200">
+        Xóa tin
+      </button>
+
+      <div className="w-full h-full flex items-end z-10">
+        <div className="bg-white/30 backdrop-blur-sm px-[12px] py-[4px] rounded-full flex items-center gap-1 text-xs font-regular text-[--color-violet] w-fit mx-auto">
+          <p>{time}</p>
+            <IoTimeOutline className="text-sm" />
+        </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default SmallStory_giang;
--- src/Pages/StoryManager.jsx
+++ src/Pages/StoryManager.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { MdFilterListAlt } from "react-icons/md";
+import Pic_giang from '../assets/pic_giang.png'
+import Pic_giang1 from '../assets/pic_giang1.png'
+import SmallStory_giang from '../Components/Story/SmallStory_giang';
+
 
 const StoryManager = () => {
   return (
-    <div>StoryManager</div>
+    <div className='p-[20px] bg-(--color-gray) flex-1 flex flex-col gap-3'>
+
+      <div className="topbar flex justify-between items-center">
+        <p className='font-bold text-[25px] text-(--color-violet)'>Quản lý tin</p>
+        <div className="filter bg-white px-[21px] py-[10px] flex gap-[10px] rounded-[20px] items-center">
+          <p className='font-semibold text-(--color-violet) text-[15px] p-0 flex items-center leading-none'>Tất cả</p>
+          <MdFilterListAlt className='font-semibold text-(--color-violet) text-[15px] leading-none'/>
+        </div>
+      </div>
+
+      <div className="feed-wrapper grid-cols-5 grid gap-5 p-[30px] bg-white rounded-[20px]">
+          <SmallStory_giang time={'3 phút trước'} storyImg={Pic_giang1} />
+          <SmallStory_giang time={'4 tiếng trước'} storyImg={Pic_giang} />
+          <SmallStory_giang time={'18 tiếng trước'} storyImg={Pic_giang1} />
+          <SmallStory_giang time={'1 ngày trước'} storyImg={Pic_giang1} />
+          <SmallStory_giang time={'1 ngày trước'} storyImg={Pic_giang1} />
+          <SmallStory_giang time={'3 ngày trước'} storyImg={Pic_giang1} />
+          <SmallStory_giang time={'4 ngày trước'} storyImg={Pic_giang1} />
+          <SmallStory_giang time={'4 ngày trước'} storyImg={Pic_giang1} />
+          <SmallStory_giang time={'5 ngày trước'} storyImg={Pic_giang1} />
+          <SmallStory_giang time={'6 ngày trước'} storyImg={Pic_giang1} />
+      </div>
+    </div>
   )
 }
 
