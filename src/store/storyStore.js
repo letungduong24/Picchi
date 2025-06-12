bc94f4a32cd6d3b153266d1657a4551e84752495
ldngduong <letungduong1624@gmail.com>
2025-06-12 15:50:26 +0700

    change story manager images

--- src/store/storyStore.js
+++ src/store/storyStore.js
@@ -1,19 +1,19 @@
 import { create } from 'zustand';
-import Pic_giang1 from '../assets/pic_giang1.png'; 
-import Pic_giang from '../assets/pic_giang.png';
+import p1 from '../assets/1.jpg';
+import p2 from '../assets/2.jpg';
 
 const useStoryStore = create((set, get) => ({
   stories: [
-    { id: 1, time: "3 phút trước", storyImg: Pic_giang1, isError: true },
-    { id: 2, time: "4 tiếng trước", storyImg: Pic_giang , isError: true },
-    { id: 3, time: "18 tiếng trước", storyImg: Pic_giang1, isError: true },
-    { id: 4, time: "1 ngày trước", storyImg: Pic_giang1, isError: true },
-    { id: 5, time: "1 ngày trước", storyImg: Pic_giang1, isError: true },
-    { id: 6, time: "3 ngày trước", storyImg: Pic_giang1, isError: false },
-    { id: 7, time: "4 ngày trước", storyImg: Pic_giang1, isError: false },
-    { id: 8, time: "4 ngày trước", storyImg: Pic_giang1, isError: false },
-    { id: 9, time: "5 ngày trước", storyImg: Pic_giang1, isError: false },
-    { id: 10, time: "6 ngày trước", storyImg: Pic_giang1, isError: false },
+    { id: 1, time: "3 phút trước", storyImg: p1, isError: true },
+    { id: 2, time: "4 tiếng trước", storyImg: p2 , isError: true },
+    { id: 3, time: "18 tiếng trước", storyImg: p1, isError: true },
+    { id: 4, time: "1 ngày trước", storyImg: p1, isError: true },
+    { id: 5, time: "1 ngày trước", storyImg: p1, isError: true },
+    { id: 6, time: "3 ngày trước", storyImg: p1, isError: false },
+    { id: 7, time: "4 ngày trước", storyImg: p1, isError: false },
+    { id: 8, time: "4 ngày trước", storyImg: p1, isError: false },
+    { id: 9, time: "5 ngày trước", storyImg: p1, isError: false },
+    { id: 10, time: "6 ngày trước", storyImg: p1, isError: false },
   ],
 
   deleteStory: (id) => {
