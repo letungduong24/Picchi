d5e1abaf56e479d9fbfd99e79f35be45dfdb8f5c
ldngduong <letungduong1624@gmail.com>
2025-06-14 20:43:12 +0700

    addfriend func, fix search

--- src/Components/Friend/SearchResult.jsx
+++ src/Components/Friend/SearchResult.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import useSuccessStore from '../../store/successStore'
 
 const SearchResult = ({id, avatar, name}) => {
+  const { showSuccess } = useSuccessStore();
+
+  const handleSendRequest = () => {
+    showSuccess({
+      title: 'Gửi lời mời kết bạn',
+      content: 'Gửi lời mời kết bạn thành công',
+      button: 'Đóng'
+    });
+  };
+
   return (
     <div className='w-full p-[20px] flex justify-between aspect-[4/1] bg-(--color-gray) rounded-[20px] items-center'>
       <div className="flex items-center gap-1">
@@ -10,7 +21,12 @@ const SearchResult = ({id, avatar, name}) => {
         <div className="font-bold text-xl">{name}</div>
       </div>
       <div className="shrink-0">
-        <button className='bg-white px-3 py-1.5 rounded-[20px] font-bold cursor-pointer'>Thêm bạn bè</button>
+        <button 
+          onClick={handleSendRequest}
+          className='bg-white px-3 py-1.5 rounded-[20px] font-bold cursor-pointer'
+        >
+          Thêm bạn bè
+        </button>
       </div>
     </div>
   )
